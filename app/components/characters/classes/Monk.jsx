6a5818d90e442ac88a9ed23df08f7aa68abc5837
byronmosley <mosleyb91@outlook.com>
2022-09-10 00:08:13 -0700

    monk class complete

--- app/components/characters/classes/Monk.jsx
+++ app/components/characters/classes/Monk.jsx
@@ -1,12 +1,87 @@
-export default function Monk({
+import { useEffect, useState } from "react";
+
+import ClassAbilityScores from "./ClassAbilityScores";
+
+import { CharacterCreationClassFeature } from "~/lib/character_creator";
+import { select } from "~/lib/common";
+
+export default function Fighter({
   character,
   setCharacter,
-  mainClass,
   subclasses,
+  feats,
   features,
-  subclassFeatures,
+  asiComplete,
   expanded,
   handleChangeExpanded,
 }) {
-  return <div>Monk</div>;
-}
+  const { level, class: { subclass } } = character;
+
+  const [featureList, setFeatureList] = useState([]);
+
+  useEffect(() => {
+    const list = features.filter((feature) => {
+      return feature.level <= level;
+    });
+
+    setFeatureList(list);
+  }, [character.class, level, features]);
+
+  const handleSubclassChange = (e) => {
+    const { value } = e.target;
+
+    setCharacter({
+      ...character,
+      class: {
+        ...character.class,
+        subclass: value,
+      },
+    });
+  };
+
+  const errors = [
+    {
+      name: "Ability Score Improvement",
+      error: !asiComplete,
+    },
+    {
+      name: "Monastic Tradition",
+      error: !subclass,
+    },
+  ];
+
+  return (
+    <>
+      {featureList.map((feature) => (
+        <CharacterCreationClassFeature
+          key={feature.index}
+          features={features}
+          index={feature.index}
+          expanded={expanded}
+          error={errors.find((error) => error.name === feature.name)}
+          handleChangeExpanded={handleChangeExpanded}
+        >
+          {feature.name === "Ability Score Improvement" && (
+            <ClassAbilityScores
+              character={character}
+              setCharacter={setCharacter}
+              level={level}
+              feats={feats}
+            />
+          )}
+          {feature.name === "Monastic Tradition" && (
+            <>
+              {select(
+                "Monastic Tradition",
+                'monastic-tradition',
+                subclass,
+                subclasses,
+                handleSubclassChange,
+              )}
+            </>
+          )}
+        </CharacterCreationClassFeature>
+      ))}
+    </>
+  );
+};
